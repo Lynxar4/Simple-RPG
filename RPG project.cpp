c0c0df62b871996081aca92f021253620ae167af
lynxar4 <lynxaar@gmail.com>
2026-05-01 19:51:24 +1000

    added enemy vectors, an area struct that contains enemies, input handling and random enemies

--- RPG project.cpp
+++ RPG project.cpp
@@ -1,109 +1,118 @@
 #include <iostream>
 #include <string_view>
+#include <vector>
+#include "Random.h"
+#include <limits>
 
 class Enemy;
 
 class Player
 {
-	int m_health{ 100 };
-	int m_attack{ 5 };
-	std::string m_name{ "Nameless" };
-
 public:
 	Player() = default;
 
-	void setName(const std::string& name)
-	{
-		m_name = name;
-	}
+	void setName(const std::string& name) { m_name = name; }
 
-	int getHealth() const
-	{
-		return m_health;
-	}
+	int getHealth() const { return m_health; }
 
-	int getAttack() const
-	{
-		return m_attack;
-	}
+	int getAttack() const { return m_attack; }
+
+	void damaged(int amount) { m_health -= amount; }
+
+	void addHealth(int amount) { m_health += amount; }
+
+	void addAttack(int amount) { m_attack += amount; }
 
 	void attack(Enemy& enemy);
 
-	void damaged(int amount)
+	void printStats() const
 	{
-		m_health -= amount;
+		std::cout << "max health: " << m_health << '\n';
+		std::cout << "attack: " << m_attack << '\n';
+		std::cout << "level: " << m_level;
 	}
 
-};
-
-enum class EnemyType
-{
-	skeleton,
-	zombie,
-	dragon,
-	wizard,
+private:
+	int m_health{ 100 };
+	int m_attack{ 5 };
+	int m_level{ 1 };
+	std::string m_name{ "Nameless" };
 };
 
 class Enemy
 {
-	int m_health{ 20 };
-	int m_attack{ 1 };
-	std::string m_name{ "Skelly" };
-	EnemyType m_type{ EnemyType::skeleton };
-
 public:
+	enum class Type
+	{
+		skeleton,
+		zombie,
+		wolf,
+		bandit,
+		wizard,
+		golem,
+		dragon,
+	};
+
 	Enemy() = default;
-	Enemy(EnemyType type)
+	Enemy(Type type)
 		:m_type{ type }
 	{
 		switch (type)
 		{
-		case(EnemyType::skeleton):
+		case(Type::skeleton):
 			m_health = 20;
 			m_attack = 10;
 			m_name = "Skeleton";
 			break;
-		case(EnemyType::zombie):
+		case(Type::zombie):
 			m_health = 15;
 			m_attack = 8;
 			m_name = "Zombie";
 			break;
-		case(EnemyType::dragon):
-			m_health = 500;
-			m_attack = 50;
-			m_name = "Dragon";
+		case(Type::wolf):
+			m_health = 20;
+			m_attack = 25;
+			m_name = "Wolf";
 			break;
-		case(EnemyType::wizard):
+		case(Type::bandit):
+			m_health = 40;
+			m_attack = 10;
+			m_name = "Bandit";
+			break;
+		case(Type::wizard):
 			m_health = 15;
 			m_attack = 20;
 			m_name = "Wizard";
 			break;
+		case(Type::golem):
+			m_health = 50;
+			m_attack = 20;
+			m_name = "Golem";
+			break;
+		case(Type::dragon):
+			m_health = 500;
+			m_attack = 50;
+			m_name = "Dragon";
+			break;
 		}
-
 	}
 
-	int getHealth() const
-	{
-		return m_health;
-	}
+	int getHealth() const { return m_health; }
 
-	int getAttack() const
-	{
-		return m_attack;
-	}
+	int getAttack() const { return m_attack; }
 
-	std::string_view getName() const
-	{
-		return m_name;
-	}
+	std::string_view getName() const { return m_name; }
+
+	void damaged(int amount) { m_health -= amount; }
 
 	void attack(Player& p);
 
-	void damaged(int amount)
-	{
-		m_health -= amount;
-	}
-
+private:
+	int m_health{ 20 };
+	int m_attack{ 1 };
+	std::string m_name{ "Skelly" };
+	Type m_type{ Type::skeleton };
+	int m_experience{ 5 };
 };
 
 void Player::attack(Enemy& enemy)
@@ -115,6 +124,18 @@ void Enemy::attack(Player& p)
 {
 	p.damaged(m_attack);
 }
+
+struct Area
+{
+	std::string m_name{};
+	std::vector<Enemy> enemies{};
+
+	Enemy getRandomEnemy() const
+	{
+		int randomEnemy{ Random::get(0, 1) };
+		return enemies[randomEnemy];
+	}
+};
 
 void versus(Player& p, Enemy e)
 {
@@ -154,7 +175,7 @@ int getChoice()
 	return choice;
 }
 
-void handleChoice(Player& p, Enemy& e, int choice)
+void handleChoice(Player& p, Enemy e, int choice)
 {
 	if (choice == 1)
 	{
@@ -166,45 +187,65 @@ void handleChoice(Player& p, Enemy& e, int choice)
 	}
 }
 
+void levelUp(Player& p)
+{
+	p.addHealth(5);
+	p.addAttack(5);
+}
+
+int getInput()
+{
+	int choice{};
+	int lowerBound{ 1 };
+	int upperBound{ 4 };
+	while (true)
+	{
+		std::cout << "Enter a number: ";
+		std::cin >> choice;
+
+		if (!std::cin)
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "That's not a number.\n";
+			continue;
+		}
+
+		if (choice < lowerBound || choice > upperBound)
+		{
+			std::cout << "Enter a valid number buddy\n";
+			continue;
+		}
+
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		return choice;
+	}
+}
+
 void game()
 {
 	Player player{};
-	Enemy zombie{ EnemyType::zombie };
-	Enemy skeleton{ EnemyType::skeleton };
-	Enemy dragon{ EnemyType::dragon };
-	Enemy wizard{ EnemyType::wizard };
+	std::vector<Enemy> villageEnemies{ Enemy{Enemy::Type::zombie}, Enemy{Enemy::Type::skeleton} };
+	std::vector<Enemy> mountainEnemies{ Enemy{Enemy::Type::wolf}, Enemy{Enemy::Type::bandit} };
+	std::vector<Enemy> ruinEnemies{ Enemy{Enemy::Type::wizard}, Enemy{Enemy::Type::golem} };
+	std::vector<Area> areas{ Area{"Village", villageEnemies}, Area{"Mountain", mountainEnemies}, Area{"Ancient Ruins", ruinEnemies} };
 
 	std::cout << "Enter your name: ";
 	std::string name{};
 	std::cin >> name;
 	player.setName(name);
-	std::cout << "Hi " << name << " welcome to simple RPG!\n";
-	std::cout << "What do you want to do mate?\n";
-	std::cout << "1. Go to the village, 2. Go to the cemetary, 3. Go to the mountains, 4. Go to the ancient ruins\n";
-	int choice{};
-	std::cin >> choice;
-	switch (choice)
+	std::cout << "Hi " << name << ", welcome to simple RPG! ";
+
+	while (player.getHealth() > 0)
 	{
-	case(1):
-		std::cout << "omg you encountered a ZOMBIE\n";
-		handleChoice(player, zombie, getChoice());
-		std::cout << "Another zombie attacks you from behind, you thought there would only be one?\n";
-		handleChoice(player, zombie, getChoice());
-		break;
-	case(2):
-		std::cout << "A spooky skeleton appears out of nowhere\n";
-		handleChoice(player, skeleton, getChoice());
-		break;
-
-	case(3):
-		std::cout << "You didn't even climb the mountain yet but a dragon flies towards you\n";
-		handleChoice(player, dragon, getChoice());
-		break;
-
-	case(4):
-		std::cout << "You reach the ruins and then get jumped by a wizard\n";
-		handleChoice(player, wizard, getChoice());
-		break;
+		std::cout << "Where do you want to go mate?\n";
+		std::cout << "1. Village\n2. Mountains\n3. Ancient ruins\n4. Quit because you're a noob\n";
+		int choice{ getInput() };
+		if (choice == 4) { return; }
+		choice -= 1;
+		std::cout << "You entered the " << areas[choice].m_name << '\n';
+		std::cout << "You encountered a " << areas[choice].getRandomEnemy().getName() << '\n';
+		handleChoice(player, areas[choice].getRandomEnemy(), getChoice());
 	}
 }
 
